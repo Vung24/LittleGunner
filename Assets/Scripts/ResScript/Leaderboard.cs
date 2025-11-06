d679ceb8a737d8dfefc3b8de6e85ae8e8bbc1efe
Vung24 <vung030524@gmail.com>
2025-11-07 05:49:52 +0700

    First Push Game

---
+++ Assets/Scripts/ResScript/Leaderboard.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using Dan.Main;
+public class Leaderboard : MonoBehaviour
+{
+    [SerializeField] private List<TextMeshProUGUI> names;
+    [SerializeField] private List<TextMeshProUGUI> scores;
+    public string publicLeaderboardKey =
+        "164ff6fdf7102f4cf83eab0b387bca39d981e1ee160eeaaee4c09ea45ea731df";
+    void Start()
+    {
+        GetLeaderboard();
+    }
+    public void GetLeaderboard()
+    {
+        LeaderboardCreator.GetLeaderboard(publicLeaderboardKey, ((msg) =>
+        {
+            int loopLength = (msg.Length < names.Count) ? msg.Length : names.Count;
+            for (int i = 0; i < loopLength; i++)
+            {
+                names[i].text = msg[i].Username;
+                scores[i].text = msg[i].Score.ToString();
+            }
+        }));
+    }
+    public void SetLeaderboardsToEntry(string username, int score)
+    {
+        LeaderboardCreator.UploadNewEntry(publicLeaderboardKey, username, score, ((msg) =>
+        {
+            GetLeaderboard();
+        }));
+    }
+}
